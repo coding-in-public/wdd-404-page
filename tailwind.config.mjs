09130eda9faf83305935e54c8c53ae2406a0c9c8
Chris Pennington <chris@codinginpublic.dev>
2024-10-27 16:04:35 -0600

    finished code

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Urbanist", system-UI, sans-serif'],
+      },
+      container: {
+        padding: "2rem",
+        center: true,
+      },
+      colors: {
+        bkg: "#F3D4A8",
+        text: "#180E0F",
+        accent: "#64748B",
+      },
+    },
+  },
+  plugins: [],
+};
